eb119f90228f568707e2524360a2b4d10c8b4102
RTC33 <rtc33@rtc33de-imac.local>
2018-08-14 13:33:50 +0800

    Initial Commit

---
+++ iOS_ClubTests/iOS_ClubTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_ClubTests.swift
+//  iOS_ClubTests
+//
+//  Created by RTC33 on 2018/8/14.
+//  Copyright © 2018年 RTC33. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Club
+
+class iOS_ClubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
